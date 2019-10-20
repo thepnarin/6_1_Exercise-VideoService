1c4228c3de34ccf570e133355f32372f4c78ce55
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 23:31:47 +0700

    Exercise-VideoService

---
+++ 6_1_Exercise-VideoService/Mocking/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_1_Exercise_VideoService.Mocking
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var service = new VideoService();
+            var title = service.ReadVideoTitle();
+        }
+    }
+}
